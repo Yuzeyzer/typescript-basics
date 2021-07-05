5ea31ccc8404410d62504f8690d9c9bff63d3427
John Doe <kurbanof.atabek@gmail.com>
2021-07-05 17:33:32 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+var person2 = 'Жантай';
+var age2 = 21;
+var user = {};
+var uluk = ['Сейтек'];
+var seitek = [1, 2, 3, 4, 5, 6, 7, 8];
+var users = [{}, {}];
+var namew = null;
---
+++ app.ts
@@ -0,0 +1,7 @@
+const person2:string = 'Жантай'
+const age2:number = 21
+const user:Object = {}
+const uluk:string[] = ['Сейтек']
+const seitek:number[] = [1,2,3,4,5,6,7,8]
+const users:Object[] = [{},{}]
+const namew:null = null
